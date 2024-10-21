441a03d478fd0be93beacd59449b88500c838a2d
Aleksandr Ziuzin <alx.zyuzin@gmail.com>
2024-10-20 20:02:56 -0700

    Create page for displaing indicators.

--- app/views.py
+++ app/views.py
@@ -8,16 +8,33 @@ from smtplib import SMTPException, SMTPConnectError, SMTPSenderRefused
 import datetime
 
 #from app.dbutils import select_imagedata
-#from app.models import db, ArtworkModel, GalleryModel
+from app.models import db, Simbol, User, UserSimbol
 from app.userlogin  import currentusername
 
 
 
 
-#======================================
+#____________________________________________________________________________
 #   Display homepage
-#======================================
+#____________________________________________________________________________
 @app.route("/")
 def home():
-   #galleries = db.session.query(GalleryModel).all()
    return render_template("home.html")
+
+
+#____________________________________________________________________________
+#   Display list of simbols and calculated indiators for selected simbol
+#____________________________________________________________________________
+@app.route("/simbols")
+def simbols():
+   if request.args:
+      req = request.args
+      if req["listtype"] == "portfolio":
+          simbols = db.session.query(Simbol).order_by(Simbol.simbol).all()
+      if req["listtype"] == "wachlist":
+          simbols = db.session.query(Simbol).order_by(Simbol.simbol).all()
+      if req["listtype"] == "unselected":
+          simbols = db.session.query(Simbol).order_by(Simbol.simbol).all()
+        
+      print(req["listtype"])
+   return render_template("simbols.html", simbols = simbols)
